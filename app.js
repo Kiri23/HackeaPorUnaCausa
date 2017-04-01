955a1b0a03c90e32e3a8d1030723da3da08aaad5
Christian Nogueras <Christiannogueras@MacBook-Pro-de-Christian.local>
2017-04-01 17:03:13 -0400

    post

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/registros', registro);
+app.use('/registro', registro);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
