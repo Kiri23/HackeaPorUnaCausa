03cb95dc623b61e33afbb86a20f94edba57ae74f
Christian Nogueras <Christiannogueras@MacBook-Pro-de-Christian.local>
2017-04-01 18:46:26 -0400

    post

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://kiri_23:Kiri1694@ds147480.mlab.com:47480/c3tec',
+  mongoURL: process.env.MONGO_URL || 'mongodb://kiri_23:Gustavo11@ds147480.mlab.com:47480/c3tec',
   port: process.env.PORT || 8000
 };
 
