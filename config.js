8f10bdae31a1cbc6ff8d21fb064a75f181aed5c3
Christian Nogueras <Christiannogueras@MacBook-Pro-de-Christian.local>
2017-04-01 18:32:18 -0400

    post

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const config = {
+var config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://kiri_23:Kiri1694@ds147480.mlab.com:47480/c3tec',
   port: process.env.PORT || 8000
 };
 
-export default config;
+module.exports = config;
